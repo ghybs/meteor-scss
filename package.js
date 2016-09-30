64d6a3377e1acb40474054dcd238d0b6b532883a
Alex <alex.borodach@gmail.com>
2016-09-30 13:03:21 +0300

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Sass and SCSS compilers for Meteor.',
-  version: '3.8.1_1',
+  version: '3.8.1_3',
   git: 'https://github.com/barbatus/meteor-scss.git',
   name: 'barbatus:scss-compiler'
 });
